6fbfc1f0888fae79b18a12a755797ff680007ab4
Lerts <oddff@mail.ru>
2020-12-05 03:56:11 +0300

    Задание 1 а.

---
+++ HomeWorkDmitriyStrelnikov(Lerts)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkDmitriyStrelnikov_Lerts_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            byte years = byte.Parse(Console.ReadLine());
+            Console.Write("Введите ваш рост: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.Write("Введите ваш вес: ");
+            float weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\tВывод спомощью склеивания.");
+            Console.WriteLine("Ваше имя: " + name + "\nВаша фамилия: " + surname + "\nВаш возраст: " + years + "\nВаш рост: " + height + "\nВаш вес: " + weight);
+
+
+            Console.ReadLine();
+        }
+    }
+}
