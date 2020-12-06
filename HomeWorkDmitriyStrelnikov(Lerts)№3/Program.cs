433b85c380df34ae252e2671746634a5a635c5b8
Lerts <oddff@mail.ru>
2020-12-06 17:47:48 +0300

    Задание 3 б

--- HomeWorkDmitriyStrelnikov(Lerts)№3/Program.cs
+++ HomeWorkDmitriyStrelnikov(Lerts)№3/Program.cs
@@ -10,30 +10,33 @@ namespace HomeWorkDmitriyStrelnikov_Lerts__3
     {
         static void Main(string[] args)
         {
-            Random rndX = new Random();
-            Random rndY = new Random();
-
-            int x1 = rndX.Next(1, 100);
-            int y1 = rndY.Next(1, 25);
+            Random rnd = new Random();
+           
+            int x1 = rnd.Next(1, 100);
+            int y1 = rnd.Next(1, 25);
             Console.SetCursorPosition(x1, y1);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("*");
-            Console.ResetColor();
+            Console.ResetColor(); 
 
-            int x2 = rndX.Next(1, 100);
-            int y2 = rndY.Next(1, 25);
+            int x2 = rnd.Next(1, 100);
+            int y2 = rnd.Next(1, 25);
             Console.SetCursorPosition(x2, y2);
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("*");
             Console.ResetColor();
 
             Console.SetCursorPosition(0, 26);
+            dictance(x1, y1, x2, y2);
+            
+            Console.ReadLine();
+        }
+
+        static void dictance(int x1, int y1, int x2, int y2)
+        {
             double r;
             r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             Console.WriteLine("Растояние между двумя точками: = {0:f2}", r);
-
-
-            Console.ReadLine();
         }
     }
 }
