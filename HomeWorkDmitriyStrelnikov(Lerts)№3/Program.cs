6f4328d7ceb06ec2ce56b5a208553b3466a84ff1
Lerts <oddff@mail.ru>
2020-12-06 03:59:50 +0300

    Задание 3 а

---
+++ HomeWorkDmitriyStrelnikov(Lerts)№3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkDmitriyStrelnikov_Lerts__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndX = new Random();
+            Random rndY = new Random();
+
+            int x1 = rndX.Next(1, 100);
+            int y1 = rndY.Next(1, 25);
+            Console.SetCursorPosition(x1, y1);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("*");
+            Console.ResetColor();
+
+            int x2 = rndX.Next(1, 100);
+            int y2 = rndY.Next(1, 25);
+            Console.SetCursorPosition(x2, y2);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("*");
+            Console.ResetColor();
+
+            Console.SetCursorPosition(0, 26);
+            double r;
+            r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Растояние между двумя точками: = {0:f2}", r);
+
+
+            Console.ReadLine();
+        }
+    }
+}
